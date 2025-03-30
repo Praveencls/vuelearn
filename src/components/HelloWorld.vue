1b026402330967237ca032a8570fe9ab16e35e96
Praveen Soni <Praveen.Soni@xcentium.com>
2025-03-30 19:11:34 +0530

    initial setup, data binding and attribute binding

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ message }}</h2>
+    <input type="text" v-model="message" />
+    <a v-bind:href="url" target="_blank">This is a link</a>
+    <a :href="url" target="_blank">This is a link2</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      message: "Hello this is data property. Vue js is reactive",
+      url: "https://vuejs.org",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
